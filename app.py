7d42e1f2daac39cde9e7d7c032afa2237a4a655e
Sandeep Sidhu <sandeep@spsidhu.com>
2013-08-01 22:59:46 +0000

    placeholders added

--- app.py
+++ app.py
@@ -3,8 +3,29 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Tech Nights!!! home page'
+
+@app.route('/schedule/')
+@app.route('/schedule/upcoming')
+def schedule_index():
+    return 'schedule placeholder.  show upcoming schedules from now() onwards'
+
+
+@app.route('/schedule/previous')
+def schedule_previous():
+    return 'schedule placeholder.  show upcoming schedules from now() onwards'
+
+
+@app.route('/schedule/add')
+def schedule_previous():
+    return 'Add new schedule placeholder '
+
+
+@app.route('/schedule/edit')
+def schedule_previous():
+    return 'Edit a schedule placeholder '
+
 
 
 if __name__ == '__main__':
